301a230529a65d236cfa61f0ae852f887ef9ad3b
Tycho van Berge Henegouwen <info@bespokeweb.nl>
2019-07-02 09:20:15 +0200

    Add blacklisting

--- example/server.js
+++ example/server.js
@@ -5,4 +5,12 @@ const ipControl = require('expressjs-ip-control')
 
 app.get('/', ipControl(), (req, res) => res.send('Hello World!'))
 
+app.get('/whitelist', ipControl({
+    whitelist: '::1'
+}), (req, res) => res.send('Hello World!'))
+
+app.get('/blacklist', ipControl({
+    blacklist: '::1'
+}), (req, res) => res.send('Hello World!'))
+
 app.listen(port, () => console.log(`Example app listening on port http://localhost${port}!`))
--- src/index.js
+++ src/index.js
@@ -2,20 +2,33 @@ require('dotenv').config()
 
 const ipControl = (settings = {}) => {
     let whitelist = process.env.EXPRESSJS_IP_WHITELIST || ''
+    let blacklist = process.env.EXPRESSJS_IP_BLACKLIST || ''
 
     if (settings.whitelist) {
         whitelist = settings.whitelist
     }
 
+    if (settings.blacklist) {
+        blacklist = settings.blacklist
+    }
+
     return (req, res, next) => {
         const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+
+        const forbidden = () => {
+            return settings.forbidden ? settings.forbidden(req, res, ip) : res.status(403).send('You do not have rights to visit this page')
+        }
 
         if (settings.exception && settings.exception()) {
             return next()
         }
 
+        if (blacklist.indexOf(ip) !== -1) {
+            return forbidden()
+        }
+
         if (whitelist.indexOf(ip) === -1) {
-            return settings.forbidden ? settings.forbidden(req, res, ip) : res.status(403).send('You do not have rights to visit this page')
+            return forbidden()
         }
 
         return next();
